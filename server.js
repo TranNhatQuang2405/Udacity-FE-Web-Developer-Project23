12c6a48e650e60ccff1004faa744f935cce2e6a5
Trần Nhất Quang <trannhatquang2405@gmail.com>
2024-04-29 17:02:25 +0700

    init node

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express")
+const bodyParser = require("body-parser")
 // Start up an instance of app
+const app = express()
+const port = 3000
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -17,3 +20,5 @@ app.use(express.static('website'));
 
 
 // Setup Server
+
+const server = app.listen(port, () => { console.log(`running on localhost: ${port}`) })
